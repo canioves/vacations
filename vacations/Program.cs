ba98c016850e400fd7adc7576c8c82df5bc81ae4
canioves <dark.gary@yandex.ru>
2024-02-18 03:31:19 +0300

    проблема с проверкой отпуска на корректность

--- vacations/Program.cs
+++ vacations/Program.cs
@@ -1,5 +1,5 @@
-﻿List<DateTime> AllVacations = new List<DateTime>();
-List<string> Workers = new List<string>()
+﻿List<DateTime> allVacations = new List<DateTime>();
+List<string> workers = new List<string>()
 {
         "Иванов Иван Иванович",
         "Петров Петр Петрович",
@@ -9,24 +9,61 @@ List<string> Workers = new List<string>()
         "Георгиев Георг Георгиевич"
 };
 
+foreach (string worker in workers)
+{
+    List<DateTime> workerVacationDates = new List<DateTime>();
+    int currentDays = 0;
+    Vacation v = new Vacation(worker);
+    int totalDays = Vacation.VacationDays;
+
+    while (currentDays < totalDays)
+    {
+        int size = totalDays - currentDays > 7 ? v.GetVacationSize() : 7;
+        bool vacationCorrect = false;
+
+        DateTime[] startAndEnd = v.GetVacationStartEnd(size);
+        List<DateTime> vacationDates = new List<DateTime>();
+
+        while (!vacationCorrect)
+        {
+            vacationDates = v.GetVacationDatesList(startAndEnd[0], startAndEnd[1]);
+            vacationCorrect = CheckIfVacationCorrect(vacationDates);
+        }
+
+        workerVacationDates = workerVacationDates.Concat(vacationDates).ToList();
+
+        currentDays += size;
+
+        bool CheckIfVacationCorrect(List<DateTime> list)
+        {
+            bool firstCheck = true;
+            bool secondCheck = true;
+            foreach (DateTime vac in list)
+            {
+                firstCheck = allVacations.Count == 0 || allVacations.All(x => x != vac.AddDays(-3));
+                secondCheck = workerVacationDates.Count == 0 || (workerVacationDates.Any(x => x.AddMonths(1) >= vac) && list.Any(x => x.AddMonths(-1) <= vac));
+            }
+
+            return firstCheck && secondCheck;
+        }
+    }
+
+    allVacations = allVacations.Concat(workerVacationDates).ToList();
+}
+
+foreach (var item in allVacations)
+{
+    Console.WriteLine(item);
+}
 
 
 class Vacation
 {
-    enum WorkingDaysOfWeek
-    {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday
-    }
+    public List<string> Weekends { get; } = new List<string> { "Saturday", "Sunday" };
     public const int VacationDays = 28;
-
-    public int[] VacationSize { get; } = [7, 14];
-    public DateTime Start { get; } = new DateTime(DateTime.Now.Year, 1, 1);
-    public DateTime End { get; } = new DateTime(DateTime.Now.Year, 12, 31);
-    public List<DateTime> WorkerVacations = new List<DateTime>();
+    private int[] VacationSize { get; } = [7, 14];
+    private DateTime Start { get; } = new DateTime(DateTime.Now.Year, 1, 1);
+    private DateTime End { get; } = new DateTime(DateTime.Now.Year, 12, 31);
     public string WorkerName;
     public Random Seed;
 
@@ -38,12 +75,30 @@ class Vacation
 
     private int GetDaysOfYear() => (End - Start).Days;
 
-    private int GetVacationSize() => VacationSize[Seed.Next(2)]; 
+    public int GetVacationSize() => VacationSize[Seed.Next(2)];
 
-    private List<DateTime> GetStartAndEndOfVacation(int range)
+    public DateTime[] GetVacationStartEnd(int vacationSize)
     {
-        DateTime vacationStart = Start.AddDays(Seed.Next(range));
-        DateTime vacationEnd = vacationStart.AddDays(GetVacationSize());
-        return new List<DateTime>();
+        int range = GetDaysOfYear();
+        DateTime vacationStart;
+        DateTime vacationEnd;
+
+        do
+        {
+            vacationStart = Start.AddDays(Seed.Next(range));
+            vacationEnd = vacationStart.AddDays(vacationSize);
+        } while (Weekends.Contains(vacationStart.DayOfWeek.ToString()));
+
+        return new DateTime[] { vacationStart, vacationEnd };
+    }
+
+    public List<DateTime> GetVacationDatesList(DateTime start, DateTime end)
+    {
+        List<DateTime> result = new List<DateTime>();
+        for (DateTime date = start; date < end; date = date.AddDays(1))
+        {
+            result.Add(date);
+        }
+        return result;
     }
 }
--- vacations/obj/Debug/net8.0/vacations.AssemblyInfo.cs
+++ vacations/obj/Debug/net8.0/vacations.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vacations")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a7ad90fa21a87920130cfa8540df6a78a1edda")]
 [assembly: System.Reflection.AssemblyProductAttribute("vacations")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vacations")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
